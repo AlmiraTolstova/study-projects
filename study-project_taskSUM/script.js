f07e55fa564db072688a617163c0b206dcd97a7a
Almira <almiratostova@gmail.com>
2025-11-30 18:45:27 +0100

    postfeed

--- study-project_taskSUM/script.js
+++ study-project_taskSUM/script.js
@@ -7,6 +7,8 @@ let listPosts = [];
 let listUsers = [];
 let listComments = [];
 
+let ListPostsWithComments = [];
+
 async function getPosts() {
   try {
     const response = await fetch(URL_POSTS);
@@ -37,9 +39,49 @@ async function getCommets() {
   }
 }
 
+function mergeData(posts, users, comments) {
+  let resultArray = [];
+  posts.forEach((post) => {
+    const currentUser = users.filter((item) => {
+      return item.id === post.userId;
+    });
+
+    const currentComments = comments.filter((item) => {
+      return item.postId === post.id;
+    });
+
+    resultArray.push({
+      postTitle: post.title,
+      userName: currentUser[0].name,
+      postBody: post.body,
+      commentsCount: currentComments.length,
+      postComments: currentComments,
+    });
+  });
+  return resultArray;
+}
+
+const listOfTasks = document.querySelector(".tasks-list");
+
+function renderPosts(arr, container) {
+  arr.forEach((element) => {
+    const postCardDiv = document.createElement("div");
+    postCardDiv.innerHTML = `
+        <h3>${element.postTitle}</h3>
+          <p class="name">${element.userName}</p>
+          <p class="post-body">${element.postBody.slice(0, 100)}...</p>
+          <p>Количество комментариев к этому посту: ${element.commentsCount}</p>
+          <button>Читать далее</button>
+    `;
+    console.log(element);
+    container.appendChild(postCardDiv);
+  });
+}
+
 window.addEventListener("load", async () => {
   listPosts = await getPosts();
   listUsers = await getUsers();
   listComments = await getCommets();
-  console.log(listPosts, listUsers, listComments);
+  ListPostsWithComments = mergeData(listPosts, listUsers, listComments);
+  renderPosts(ListPostsWithComments, listOfTasks);
 });
